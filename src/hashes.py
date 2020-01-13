5ced716725aee41abcebb5676a4ac68167c54a0a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-13 13:50:16 -0500

    Adds a new 'hashes.py' file

---
+++ src/hashes.py
@@ -0,0 +1,26 @@
+import time
+import hashlib
+
+
+n = 1000000
+key = b"STR"
+
+for _ in range(10):
+  print(hash(key)) # Python's built-in 'hash()' function
+print("\n")
+  
+for _ in range(10):
+  print(hashlib.sha256(key)) # sha256 hash function
+print("\n")
+  
+for _ in range(10):
+  print(hashlib.sha256(key).hexdigest()) # enable visualization of hash
+print("\n")
+
+for _ in range(10):
+  print(int(hashlib.sha256(key).hexdigest(), 16)) # enable visualization of 16 digits of hash
+print("\n")
+
+# for _ in range(10):
+#   print(int(hashlib.sha256(key).hexdigest(), (16 % 8)) # enable visualization of hash
+# print("\n")
